0b9aa995fc0471a3af2ce9b6eca3c15460b41d9d
bogdanmaizel <mikeybx10@gmail.com>
2022-05-03 20:04:30 +0300

    Logs for OrderController

--- src/main/java/sd/a2/assignment2/rest/OrderController.java
+++ src/main/java/sd/a2/assignment2/rest/OrderController.java
@@ -2,6 +2,9 @@ package sd.a2.assignment2.rest;
 
 import java.util.List;
 import javax.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,31 +22,33 @@ import sd.a2.assignment2.service.OrderService;
 
 @RestController
 @RequestMapping(value = "/api/orders", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
-    public OrderController(final OrderService orderService) {
-        this.orderService = orderService;
-    }
-
     @GetMapping("/from-user/{id}")
     public ResponseEntity<?> getOrdersFromCustomer(@PathVariable final Long id) {
+        log.info("API access - Fetching orders for customer, ID {}", id);
         return ResponseEntity.ok(orderService.findFromCustomer(id));
     }
 
     @GetMapping("/from-restaurant/{id}")
     public ResponseEntity<?> getOrdersFromRestaurant(@PathVariable final Long id) {
+        log.info("API access - Fetching orders for restaurant, ID {}", id);
         return ResponseEntity.ok(orderService.findFromRestaurant(id));
     }
 
     @PostMapping
     public ResponseEntity<Long> createOrder(@RequestBody @Valid final OrderDTO orderDTO) {
+        log.info("API access - Creating order from customer {} to restaurant {}", orderDTO.getCustomer(), orderDTO.getRestaurant());
         return new ResponseEntity<>(orderService.create(orderDTO), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}/{advanceOrCancel}")
     public ResponseEntity<Void> updateOrder(@PathVariable final Long id, @PathVariable int advanceOrCancel) {
+        log.info("API access - Updating order status, ID {}", id);
         orderService.update(id, advanceOrCancel);
         return ResponseEntity.ok().build();
     }
