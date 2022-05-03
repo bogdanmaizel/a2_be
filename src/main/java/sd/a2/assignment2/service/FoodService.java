935e96c58058a596ca3d223759c04d25996576dc
bogdanmaizel <mikeybx10@gmail.com>
2022-05-03 10:46:03 +0300

    Logs+Javadoc FoodService

--- src/main/java/sd/a2/assignment2/service/FoodService.java
+++ src/main/java/sd/a2/assignment2/service/FoodService.java
@@ -1,9 +1,7 @@
 package sd.a2.assignment2.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,40 +11,71 @@ import sd.a2.assignment2.model.FoodDTO;
 import sd.a2.assignment2.repos.FoodRepository;
 import sd.a2.assignment2.repos.RestaurantRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Handles various logic and operations concerning the food entities.
+ */
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class FoodService {
 
     private final FoodRepository foodRepository;
     private final RestaurantRepository restaurantRepository;
 
-    @Autowired
-    public FoodService(final FoodRepository foodRepository,
-                       final RestaurantRepository restaurantRepository) {
-        this.foodRepository = foodRepository;
-        this.restaurantRepository = restaurantRepository;
-    }
+    /**
+     * Fetch all the foods currently in the database.
+     * @return A list of all the foods.
+     */
 
     public List<FoodDTO> findAll() {
+        log.info("Fetching food list");
         return foodRepository.findAll()
                 .stream()
                 .map(food -> mapToDTO(food, new FoodDTO()))
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Search for a food by its ID.
+     * @param id The food ID.
+     * @return The corresponding food, mapped to a DTO.
+     */
+
     public FoodDTO get(final Long id) {
+        log.info("Fetching food with ID {}", id);
         return foodRepository.findById(id)
                 .map(food -> mapToDTO(food, new FoodDTO()))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> {
+                    log.warn("Food with ID {} not found", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Food not found");
+                });
     }
 
+    /**
+     * Fetch the menu of a restaurant.
+     * @param id The ID of the restaurant.
+     * @return The list of foods associated to the restaurant.
+     */
+
     public List<FoodDTO> getMenu(final Long id) {
+        log.info("Fetching menu for restaurant {}", id);
         return foodRepository.findByRestaurantId(id).stream()
                 .map(food -> mapToDTO(food, new FoodDTO()))
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Creates a new food entity, based on the parameters given in the DTO.
+     * @param foodDTO the DTO containing the information about the food object.
+     * @return A DTO reflecting the received information, completed with an ID.
+     */
+
     public FoodDTO create(final FoodDTO foodDTO) { // builder pattern
+        log.info("Adding food item - {}", foodDTO.getName());
         final Food food = Food.builder()
                 .name(foodDTO.getName())
                 .description(foodDTO.getDescription())
@@ -70,7 +99,15 @@ public class FoodService {
         foodRepository.deleteById(id);
     }
 
+    /**
+     * Mapper method: creates a DTO from an entity.
+     * @param food The entity to be converted.
+     * @param foodDTO The resulting DTO.
+     * @return The resulting DTO.
+     */
+
     private FoodDTO mapToDTO(final Food food, final FoodDTO foodDTO) {
+        log.info("Creating DTO for food {}", food.getName());
         foodDTO.setId(food.getId());
         foodDTO.setName(food.getName());
         foodDTO.setDescription(food.getDescription());
@@ -80,14 +117,25 @@ public class FoodService {
         return foodDTO;
     }
 
+    /**
+     * Mapper method: creates an entity from a given DTO
+     * @param foodDTO The DTO to be converted.
+     * @param food The resulting entity.
+     * @return The resulting entity.
+     */
+
     private Food mapToEntity(final FoodDTO foodDTO, final Food food) {
+        log.info("Creating entity for food {}", foodDTO.getName());
         food.setName(foodDTO.getName());
         food.setDescription(foodDTO.getDescription());
         food.setPrice(foodDTO.getPrice());
         food.setCategory(foodDTO.getCategory());
         if (foodDTO.getRestaurant() != null && (food.getRestaurant() == null || !food.getRestaurant().getId().equals(foodDTO.getRestaurant()))) {
             final Restaurant restaurant = restaurantRepository.findById(foodDTO.getRestaurant())
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "restaurant not found"));
+                    .orElseThrow(() -> {
+                        log.warn("Restaurant with ID {} not found", foodDTO.getRestaurant());
+                        return new ResponseStatusException(HttpStatus.NOT_FOUND, "Restaurant not found");
+                    });
             food.setRestaurant(restaurant);
         }
         return food;
