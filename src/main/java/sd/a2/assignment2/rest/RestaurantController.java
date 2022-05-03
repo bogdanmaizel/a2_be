0e1ad54795dca3b736c8c437c33d20d62f2bf168
bogdanmaizel <mikeybx10@gmail.com>
2022-05-03 20:08:02 +0300

    Logs for RestaurantController

--- src/main/java/sd/a2/assignment2/rest/RestaurantController.java
+++ src/main/java/sd/a2/assignment2/rest/RestaurantController.java
@@ -1,45 +1,41 @@
 package sd.a2.assignment2.rest;
 
-import java.util.List;
-import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sd.a2.assignment2.model.RestaurantDTO;
 import sd.a2.assignment2.service.RestaurantService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
 @RequestMapping(value = "/api/restaurants", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+@Slf4j
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
-    public RestaurantController(final RestaurantService restaurantService) {
-        this.restaurantService = restaurantService;
-    }
-
     @GetMapping
     public ResponseEntity<List<RestaurantDTO>> getAllRestaurants() {
+        log.info("API access - Fetching restaurants list");
         return ResponseEntity.ok(restaurantService.findAll());
     }
 
     @GetMapping("/admin-{adminId}")
     public ResponseEntity<?> getRestaurantByAdminId(@PathVariable final Long adminId) {
+        log.info("API access - Fetching restaurant for admin, ID {}", adminId);
         return ResponseEntity.ok(restaurantService.getFromAdmin(adminId));
     }
 
     @PostMapping
-    public ResponseEntity<?> createRestaurant(
-            @RequestBody @Valid final RestaurantDTO restaurantDTO) {
+    public ResponseEntity<?> createRestaurant(@RequestBody @Valid final RestaurantDTO restaurantDTO) {
+        log.info("API access - Creating restaurant {} for admin, ID {}", restaurantDTO.getName(), restaurantDTO.getAdmin());
         return new ResponseEntity<>(restaurantService.create(restaurantDTO), HttpStatus.CREATED);
     }
 
