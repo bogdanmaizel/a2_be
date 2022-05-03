5e7bf1d8c5cb702c0ef0b2323e069fb0c21367ff
bogdanmaizel <mikeybx10@gmail.com>
2022-05-03 10:41:04 +0300

    Logs+Javadoc CustomerService

--- src/main/java/sd/a2/assignment2/service/CustomerService.java
+++ src/main/java/sd/a2/assignment2/service/CustomerService.java
@@ -2,6 +2,9 @@ package sd.a2.assignment2.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,18 +14,17 @@ import sd.a2.assignment2.model.CustomerDTO;
 import sd.a2.assignment2.repos.CustomerRepository;
 import sd.a2.assignment2.repos.UserRepository;
 
+/**
+ * Handles various logic and operations concerning the customers.
+ */
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final UserRepository userRepository;
-
-    public CustomerService(final CustomerRepository customerRepository,
-            final UserRepository userRepository) {
-        this.customerRepository = customerRepository;
-        this.userRepository = userRepository;
-    }
 
     public List<CustomerDTO> findAll() {
         return customerRepository.findAll()
@@ -31,21 +33,41 @@ public class CustomerService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Returns customer entity based on ID.
+     * @param id The customer ID.
+     * @return A DTO with the customer information.
+     */
+
     public CustomerDTO get(final Long id) {
+        log.info("Fetching customer {}", id);
         return customerRepository.findById(id)
                 .map(customer -> mapToDTO(customer, new CustomerDTO()))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> {
+                    log.warn("Customer {} not found, no changes made", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+                });
     }
+
+    /**
+     * Create a customer entity.
+     * @param customerDTO Customer data.
+     * @return Customer ID.
+     */
 
     public Long create(final CustomerDTO customerDTO) {
         final Customer customer = new Customer();
         mapToEntity(customerDTO, customer);
+        log.info("Created customer {}", customerDTO.getCustomer());
         return customerRepository.save(customer).getId();
     }
 
     public void update(final Long id, final CustomerDTO customerDTO) {
         final Customer customer = customerRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> {
+                    log.warn("Customer {} not found, no changes made", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+                });
         mapToEntity(customerDTO, customer);
         customerRepository.save(customer);
     }
@@ -54,15 +76,34 @@ public class CustomerService {
         customerRepository.deleteById(id);
     }
 
+    /**
+     * Maps entities to DTOs.
+     * @param customer The entity.
+     * @param customerDTO The DTO.
+     * @return Customer DTO.
+     */
+
     private CustomerDTO mapToDTO(final Customer customer, final CustomerDTO customerDTO) {
+        log.info("Create DTO from entity {}", customer.getId());
         customerDTO.setCustomer(customer.getUser() == null ? null : customer.getUser().getId());
         return customerDTO;
     }
 
+    /**
+     * Maps DTOs to entities.
+     * @param customerDTO The DTO.
+     * @param customer The entity.
+     * @return Customer entity.
+     */
+
     private Customer mapToEntity(final CustomerDTO customerDTO, final Customer customer) {
+        log.info("Create entity from DTO {}", customerDTO.getCustomer());
         if (customerDTO.getCustomer() != null && (customer.getUser() == null || !customer.getUser().getId().equals(customerDTO.getCustomer()))) {
             final User user = userRepository.findById(customerDTO.getCustomer())
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found"));
+                    .orElseThrow(() -> {
+                        log.warn("Customer {} not found, no changes made", customerDTO.getCustomer());
+                        return new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+                    });
             customer.setUser(user);
         }
         return customer;
